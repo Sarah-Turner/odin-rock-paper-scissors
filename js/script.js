7daab4434dac258fd712e5ef24d134447951ee5a
Sarah-Turner <81490363+Sarah-Turner@users.noreply.github.com>
2026-02-04 14:08:52 -0800

    Add functionality to play a single round
    A single round can be played between the human and computer when
    the site loads.

--- js/script.js
+++ js/script.js
@@ -17,7 +17,37 @@ function getComputerChoice() {
 
 function getUserChoice() {
     let userChoice = prompt("Enter rock, paper, or scissors: ");
-    return userChoice;
+    return userChoice.toLowerCase();
 }
 
-console.log(getUserChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock!");
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors!");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats Paper!");
+        computerScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock!");
+        humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats Scissors!");
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats Paper!");
+        humanScore++;
+    } else {
+        console.log("Tie!");
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanChoice = getUserChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
