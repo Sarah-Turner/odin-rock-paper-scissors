e02236efbca037477774e6209787a0b2448b9f83
Sarah-Turner <81490363+Sarah-Turner@users.noreply.github.com>
2026-02-19 16:21:07 -0800

    Add button functionality
    Can play rock, paper, scissors using the buttons instead of prompt.
    Buttons will select players choice and play a single round.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     let computerChoice = "";
@@ -21,51 +24,57 @@ function getUserChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    let humanWins = null;
+    let outcome = 0;
     if (humanChoice === "rock" && computerChoice === "paper") {
         console.log("You lose! Paper beats Rock!");
-        humanWins = false;
+        outcome = -1;
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
         console.log("You win! Rock beats Scissors!");
-        humanWins = true;
+        outcome = 1;
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
         console.log("You lose! Scissors beats Paper!");
-        humanWins = false;
+        outcome = -1;
     } else if (humanChoice === "paper" && computerChoice === "rock") {
         console.log("You win! Paper beats Rock!");
-        humanWins = true;
+        outcome = 1;
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
         console.log("You lose! Rock beats Scissors!");
-        humanWins = false;
+        outcome = -1;
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
         console.log("You win! Scissors beats Paper!");
-        humanWins = true;
+        outcome = 1;
     } else {
         console.log("Tie!");
     }
-    return humanWins;
+    return outcome;
+}
+
+function pressRock(event) {
+    let outcome = playRound("rock", getComputerChoice());
+    humanScore += outcome;
+    computerScore -= outcome;
+}
+
+function pressPaper(event) {
+    let outcome = playRound("paper", getComputerChoice());
+    humanScore += outcome;
+    computerScore -= outcome;
+}
+
+function pressScissors(event) {
+    let outcome = playRound("scissors", getComputerChoice());
+    humanScore += outcome;
+    computerScore -= outcome;
 }
 
 function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const humanChoice = getUserChoice();
-        const computerChoice = getComputerChoice();
-        let humanWins = playRound(humanChoice, computerChoice);
-        if (humanWins !== null && humanWins) {
-            humanScore++;
-        } else if (humanWins !== null && !humanWins) {
-            computerScore++;
-        }
-    }
-    if (humanScore > computerScore) {
-        console.log("Human Wins!");
-    } else if (computerScore > humanScore) {
-        console.log("Computer Wins!");
-    } else {
-        console.log("No one Wins!");
-    }
+    const rockBtn = document.querySelector(".rock");
+    const paperBtn = document.querySelector(".paper");
+    const scissorsBtn = document.querySelector(".scissors");
+
+    rockBtn.addEventListener("click", pressRock);
+    paperBtn.addEventListener("click", pressPaper);
+    scissorsBtn.addEventListener("click", pressScissors);
 }
 
 playGame();
